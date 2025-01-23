0d2be4dd09f3347c7830befb7baddca52d97238b
Vikram Dhumal <dhumalv310@gmail.com>
2025-01-23 07:32:29 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="home.css">
+</head>
+<body>
+<?php include("header.php");?>
+       <div class="slider">
+            <figure>
+              <div class="slide">
+                <img src="home1/hero-bedshit.gif"alt="hero img">
+              </div>
+            </figure>
+      </div>
+
+      <div class="image-container-home">
+        <div class="image-box-home">
+          <img src="home1/bedshit.1.jpg" alt="Image 1">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/bedshit.2.jpg" alt="Image 2">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/bedshit.5.jpg" alt="Image 3">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/bedshit.4.jpg" alt="Image 4">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/bedshit.3.jpg" alt="Image 5">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/bedshit.6.jpg" alt="Image 6">
+          <span>Shop Now</span>
+        </div>
+      </div>
+      
+      <div class="slider" id="div2">
+        <figure>
+          <div class="slide">
+            <img src="home1/hero-home.jpg"alt="hero img">
+          </div>
+          </figure>
+      </div>
+      <div class="image-container-home">
+        <div class="image-box-home">
+          <img src="home1/home.1.jpg" alt="Image 1">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home.2.jpg" alt="Image 2">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home.3.jpg" alt="Image 3">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home.4.jpg" alt="Image 4">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home.5.jpg" alt="Image 5">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home.6.jpg" alt="Image 6">
+          <span>Shop Now</span>
+        </div>
+      </div>
+
+
+      <div class="bath" id="div3">
+      <div class="slider1">
+        <figure>
+          <div class="slide1">
+            <img src="home1/hero-home-1.jpg"alt="hero img">
+          </div>
+          </figure>
+      </div>
+      <div class="image-container-home1">
+        <div class="image-box-home1">
+          <img src="home1/home.7.jpg" alt="Image 1">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home1">
+          <img src="home1/home.8.jpg" alt="Image 2">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home1">
+          <img src="home1/home.9.jpg" alt="Image 3">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home1">
+          <img src="home1/home.10.jpg" alt="Image 4">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home1">
+          <img src="home1/home.11.jpg" alt="Image 5">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home1">
+          <img src="home1/home.12.jpg" alt="Image 6">
+          <span>Shop Now</span>
+        </div>
+      </div>
+      </div>
+
+
+      <div class="slider" id="div4">
+        <figure>
+          <div class="slide">
+            <img src="home1/hero-home-2.gif"alt="hero img">
+          </div>
+          </figure>
+      </div>
+      <div class="image-container-home">
+        <div class="image-box-home">
+          <img src="home1/home13.jpg" alt="Image 1">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home14.jpg" alt="Image 2">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home15.jpg" alt="Image 3">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home16.jpg" alt="Image 4">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home17.jpg" alt="Image 5">
+          <span>Shop Now</span>
+        </div>
+        <div class="image-box-home">
+          <img src="home1/home18.jpg" alt="Image 6">
+          <span>Shop Now</span>
+        </div>
+      </div>
+      <br><br><hr><br>
+      <?php include("footer.php")?>
+</body>
+</html>
